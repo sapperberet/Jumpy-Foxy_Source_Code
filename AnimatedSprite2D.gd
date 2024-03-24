5c3b0b0d1a256560ae478698e62ee93360fd607d
Ahmed Omran <ahmedamromran2003@gmail.com>
2024-03-24 17:30:54 +0200

    re lock #2

---
+++ AnimatedSprite2D.gd
@@ -0,0 +1,10 @@
+extends AnimatedSprite2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	velocity.y += gravity  * delta 
+	move_and_slide()
+func _on_player_detection_body_entered(body):
+	if body.name == "Player":
+		
